8c066e2f87c1888f1700b333572200954a6e68e2
Yash <110482179+Yash35945@users.noreply.github.com>
2025-07-29 10:37:44 +0530

    commit

---
+++ backend/hardware-shop/src/main/java/com/yash/hardware_shop/HardwareShopApplication.java
@@ -0,0 +1,13 @@
+package com.yash.hardware_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardwareShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HardwareShopApplication.class, args);
+	}
+
+}
---
+++ backend/hardware-shop/src/test/java/com/yash/hardware_shop/HardwareShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yash.hardware_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardwareShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
